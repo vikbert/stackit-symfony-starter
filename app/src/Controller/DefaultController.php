66717eb76724a5cfc5f4d0d0f6e0bfba5b39da36
Xun Zhou <xun.zhou@mail.schwarz>
2022-02-05 09:16:08 +0100

    updates

---
+++ app/src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class DefaultController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', []);
+    }
+}
